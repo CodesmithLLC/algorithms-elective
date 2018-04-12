c36bc222ae690303a6c79f39bd21633ff44f9eb5
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2018-04-12 08:38:23 -0400

    added day two challenges

---
+++ day-two/dynamic-programming.js
@@ -0,0 +1,37 @@
+// Maximum Value Path
+
+function maxValuePath(coinGrid) {
+
+}
+
+// Longest Common Subsequence
+
+function LCS(x, y) {
+
+}
+
+// Golomb Sequence
+
+function golomb(n) {
+
+}
+
+
+// Fair Split
+
+function fairSplit(x) {
+
+}
+
+// Best Strategy
+
+function bestStrategy(coins) {
+
+}
+
+// Extension: Shortest Path in DAG
+// (first create a Directed Graph data structure)
+
+function shortestPath(s, v) {
+
+}
